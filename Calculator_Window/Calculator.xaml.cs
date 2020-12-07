6849ae6a3989b20ee37a55208b8e82a4656e5a70
BoolPurist <fnaumann@posteo.de>
2020-12-08 00:43:45 +0100

    Wrote xaml for prototype gui of calculator control

---
+++ Calculator_Window/Calculator.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator_Window
+{
+  /// <summary>
+  /// Interaction logic for Calculator.xaml
+  /// </summary>
+  public partial class Calculator : UserControl
+  {
+    public Calculator()
+    {
+      InitializeComponent();
+      
+
+    }
+
+    private void CalculationBtn_Click(object sender, RoutedEventArgs e)
+    {
+      Debug.WriteLine("Button was pressed !");
+    }
+
+    private void CalculationBtn_Loaded(object sender, RoutedEventArgs e)
+    {
+      Debug.WriteLine("Loaded");
+    }
+  }
+}
