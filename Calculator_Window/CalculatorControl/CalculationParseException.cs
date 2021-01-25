e469f4e699447c0c96a4e631314d068d68c4b5e5
BoolPurist <fnaumann@posteo.de>
2021-01-25 01:58:05 +0100

    Removed unsed namespaces

--- Calculator_Window/CalculatorControl/CalculationParseException.cs
+++ Calculator_Window/CalculatorControl/CalculationParseException.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Calculator_Window.CalculatorControl;
 
 namespace Calculator_Window
--- Calculator_Window/CalculatorControl/Calculator.xaml.cs
+++ Calculator_Window/CalculatorControl/Calculator.xaml.cs
@@ -1,18 +1,10 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Text;
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 using Calculator_Window.Util;
 using Calculator_Window.CalculatorControl;
